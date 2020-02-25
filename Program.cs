68a17ce417bb36ea02eabeee29c23f836cc9bfb1
xqitong <xqit@outlook.com>
2020-02-25 14:05:23 +0000

    add MyDelegate namespace

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using MyDelegate;
 namespace workspace
 {
     class Program
@@ -22,51 +22,6 @@ namespace workspace
             
         }
     }
-    class Product
-    {
-        public string Name{get;set;}
-        public double Price{get;set;}
-    }
-    class Logger
-    {
-        public void Log(Product product){
-        Console.WriteLine("Product '{0}' created at {1}.Price is {2}.",product.Name,DateTime.UtcNow,product.Price);
-        }
-    }
-    class Box
-    {
-        public Product Product{get;set;}
-    }e
-    class WrapFactory
-    {
-        public Box WrapProduct(Func<Product> getProduct,Action<Product> logCallBack)
-        {
-            Box box = new Box();
-            Product product = getProduct.Invoke();
-            if(product.Price >= 50)
-            {
-                logCallBack(product);
-            }
-            box.Product = product;
-            return box;
-        }
-    }
-    class ProductFactory
-    {
-        public Product MakePizza()
-        {
-            Product product = new Product();
-            product.Name = "Pizza";
-            product.Price = 12;
-            return product;
-        }
-        public Product MakeToyCar()
-        {
-            Product product = new Product();
-            product.Name = "ToyCar";
-            product.Price = 100;
-            return product;
-        }
-    }
+   
 }
 
