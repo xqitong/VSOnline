e099c553075576f84a97e6036fca9d79f1512369
xqitong <xqit@outlook.com>
2020-02-26 06:41:17 +0000

    MultiDelegateExample

---
+++ MulticastDelegate.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace MulticastDelegate{
+class Student
+{
+    public int ID{get;set;}
+    public ConsoleColor PenColor {get;set;}
+
+     public void DoHomeWork()
+     {
+        //Console.ForegroundColor = this.PenColor;
+        for(int i = 0; i < 5; ++i)
+        {
+            Console.ForegroundColor = this.PenColor;
+            Console.WriteLine("Student {0} doing homework {1} hour(s).",this.ID,i);
+            Thread.Sleep(1000);
+        }
+     }
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using MyDelegate;
+using MulticastDelegate;
 namespace workspace
 {
     class Program
@@ -7,18 +8,28 @@ namespace workspace
         static void Main(string[] args)
         {
             
-            ProductFactory productFactory = new ProductFactory();
-            WrapFactory wrapFactory = new WrapFactory();
-            Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
-            Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+            //ProductFactory productFactory = new ProductFactory();
+            //WrapFactory wrapFactory = new WrapFactory();
+            //Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+            //Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
 
-            Logger logger= new Logger();
-            Action<Product> log = new Action<Product>(logger.Log);
+            //Logger logger= new Logger();
+            //Action<Product> log = new Action<Product>(logger.Log);
 
-            Box box1 = wrapFactory.WrapProduct(func1,log);
-            Box box2 = wrapFactory.WrapProduct(func2,log);
-            Console.WriteLine(box1.Product.Name);
-            Console.WriteLine(box2.Product.Name);
+            //Box box1 = wrapFactory.WrapProduct(func1,log);
+            //Box box2 = wrapFactory.WrapProduct(func2,log);
+            //Console.WriteLine(box1.Product.Name);
+            //Console.WriteLine(box2.Product.Name);
+            //
+            Student stu1 = new Student(){ID = 1, PenColor = ConsoleColor.Yellow};
+            Student stu2 = new Student(){ID = 2, PenColor = ConsoleColor.Green};
+            Student stu3 = new Student(){ID = 3, PenColor = ConsoleColor.Red};
+            Action action1 = new Action(stu1.DoHomeWork);
+            Action action2 = new Action(stu2.DoHomeWork);
+            Action action3 = new Action(stu3.DoHomeWork);
+            action1 += action2;
+            action1 += action3;
+            action1.Invoke();
             
         }
     }
