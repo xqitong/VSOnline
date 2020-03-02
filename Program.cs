b0c34f6d472f66b3f9bab92895d676ef0379ad29
xqitong <xqit@outlook.com>
2020-03-02 07:49:03 +0000

    mod files

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using MyDelegate;
 using MulticastDelegate;
 using System.Threading;
+using System.Threading.Tasks;
 namespace workspace
 {
     class Program
@@ -25,21 +26,33 @@ namespace workspace
             Student stu1 = new Student(){ID = 1, PenColor = ConsoleColor.Yellow};
             Student stu2 = new Student(){ID = 2, PenColor = ConsoleColor.Green};
             Student stu3 = new Student(){ID = 3, PenColor = ConsoleColor.Red};
-            Action action1 = new Action(stu1.DoHomeWork);
-            Action action2 = new Action(stu2.DoHomeWork);
-            Action action3 = new Action(stu3.DoHomeWork);
-            //
+            //Action action1 = new Action(stu1.DoHomeWork);
+            //Action action2 = new Action(stu2.DoHomeWork);
+            //Action action3 = new Action(stu3.DoHomeWork);
+            // 在虚拟机上 action BeginInvoke 不好用
             //action1.BeginInvoke(null,null);
             //action2.BeginInvoke(null,null);
-            //action2.BeginInvoke(null,null);
             //action3.BeginInvoke(null,null);
+            
+            // Thread 是好用的 
+            //Thread th1 = new Thread(new ThreadStart(stu1.DoHomeWork));
+            //Thread th2 = new Thread(new ThreadStart(stu2.DoHomeWork));
+            //th1.Start();
+            //th2.Start(); 
+            //task
+            //Task ts1 = new Task(new Action(stu1.DoHomeWork));
+            //Task ts2 = new Task(new Action(stu2.DoHomeWork));
+            //Task ts3 = new Task(new Action(stu3.DoHomeWork));
+            //ts1.Start();
+            //ts2.Start();
+            //ts3.Start();
 
-            for(int i = 0; i <10; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Main thread{0}",i);
-                Thread.Sleep(1000);
-            }
+            //for(int i = 0; i <10; i++)
+           // {
+             //   Console.ForegroundColor = ConsoleColor.Cyan;
+             //   Console.WriteLine("Main thread{0}",i);
+             //   Thread.Sleep(1000);
+           // }
             
         }
     }
