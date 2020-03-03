b99cb331781177ca0b8a15e57846189f6d74a700
xqitong <xqit@outlook.com>
2020-03-03 12:10:29 +0000

    update

--- MyEvent.cs
+++ MyEvent.cs
@@ -1,4 +1,21 @@
+using System;
+using System.Timers;
 namespace MyEvent{
 
-    
+    class Boy
+    {
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("timer is calling me");
+            //throw new NotImplementedException();
+        }
+    }
+    class Girl
+    {
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("sing a song");
+            //throw new NotImplementedException();
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using System;
-using MyDelegate;
-using MulticastDelegate;
-using System.Threading;
-using System.Threading.Tasks;
+//using MyDelegate;
+//using MulticastDelegate;
+//using System.Threading;
+//using System.Threading.Tasks;
+using System.Timers;
+using MyEvent;
 namespace workspace
 {
     class Program
@@ -23,9 +25,9 @@ namespace workspace
             //Console.WriteLine(box1.Product.Name);
             //Console.WriteLine(box2.Product.Name);
             //
-            Student stu1 = new Student(){ID = 1, PenColor = ConsoleColor.Yellow};
-            Student stu2 = new Student(){ID = 2, PenColor = ConsoleColor.Green};
-            Student stu3 = new Student(){ID = 3, PenColor = ConsoleColor.Red};
+            //Student stu1 = new Student(){ID = 1, PenColor = ConsoleColor.Yellow};
+            //Student stu2 = new Student(){ID = 2, PenColor = ConsoleColor.Green};
+            //Student stu3 = new Student(){ID = 3, PenColor = ConsoleColor.Red};
             //Action action1 = new Action(stu1.DoHomeWork);
             //Action action2 = new Action(stu2.DoHomeWork);
             //Action action3 = new Action(stu3.DoHomeWork);
@@ -53,7 +55,26 @@ namespace workspace
              //   Console.WriteLine("Main thread{0}",i);
              //   Thread.Sleep(1000);
            // }
+
+            //Timer timer = new Timer();
+            //timer.Interval = 1000;
+            ////小闪电 代表  事件：通知别人
+            ////小扳手 代表  属性：存储数据
+            ////小方块 代表  方法：做事情
+            //Boy boy = new Boy();
+            ////事件五个部分
+            ////事件的拥有者 timer
+            ////事件 elasped
+            ////事件的响应者  boy
+            ////事件处理器 boy.action
+            ////事件订阅 += 操作符
+            //timer.Elapsed += boy.Action;//事件订阅  +=左边是事件，右边是事件处理器
+            //Girl girl = new Girl();
+            //timer.Elapsed += girl.Action;
+            //timer.Start();
+            //Console.ReadLine();
             
+
         }
     }
    
